49b0a15274c33b17cecc874c5c5a75069c1f81e2
Lohit Bura <lohitbura@gmail.com>
2019-02-26 00:49:56 +0530

    added edit option in profile

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -12,6 +12,50 @@ const authCheck = (req,res,next)=>
     }
 };
 
+router.get('/edit',authCheck,(req,res)=>{
+    data = req.query.data;
+    value = req.query.value;
+   console.log("value is",value);
+    console.log("data is",data);
+   if(value==null || value == "")
+   {
+        
+   }
+   else{
+    User.findById(req.user.username, function (err, user) {
+
+        console.log(req.user.username);
+
+        if(data=="name")
+        {
+        req.user.set({name : value})
+        }
+        if(data=="dob")
+        {
+            req.user.set({dob : value})
+        }
+        if(data=="email")
+        {
+            req.user.set({email : value})
+        }
+        if(data=="gender")
+        {
+            req.user.set({gender : value})
+        }
+        if(data=="mobile")
+        {
+            req.user.set({mobile : value})   
+        }
+       
+        req.user.save(function (err, updatedTank) {
+            
+           
+        });
+    });
+}
+    
+
+})
 
 router.get('/',authCheck,(req,res)=>{
     console.log('aa gya yha to');
